ba027fa152868b1dc7389490d268c96a4636b369
Lili Huynh <lillianhuynh312@gmail.com>
2020-09-22 10:27:21 -0700

    define var for characters

--- Develop/script.js
+++ Develop/script.js
@@ -12,15 +12,15 @@
 // * Gather user input with prompt's and confirm's
 
 function generatePassword() {
-  
+
     var range = 0;
-  
+
     while (range < 8 || range > 128) {
-      var rangeChar = prompt("How many characters would you like your password to contain (8-128 characters)?");
-      range = parseInt(rangeChar);
-      if (range < 8 || range > 128) {
-        alert("Please choose password length from 8-128 characters");
-      }
+        var rangeChar = prompt("How many characters would you like your password to contain (8-128 characters)?");
+        range = parseInt(rangeChar);
+        if (range < 8 || range > 128) {
+            alert("Please choose password length from 8-128 characters");
+        }
     }
 
     var confirmSpecial = confirm("Click OK to confirm including special characters");
@@ -28,16 +28,20 @@ function generatePassword() {
     var confirmLowercase = confirm("Click OK to confirm including lowercase characters");
     var confirmUppercase = confirm("Click OK to confirm including uppercase characters");
     var newpassword = "";
-  
+
     while (confirmSpecial === false && confirmNumeric === false && confirmLowercase === false && confirmUppercase === false) {
         alert("Please choose at least one character type");
         confirmSpecial = confirm("Click OK to confirm including special characters");
         confirmNumeric = confirm("Click OK to confirm including numeric characters");
         confirmLowercase = confirm("Click OK to confirm including lowercase characters");
         confirmUppercase = confirm("Click OK to confirm including uppercase characters");
-      }
+    }
 
-
+    var number = "0123456789";
+    var lowercase = "abcdefghijklmnopqrstuvwxyz";
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var special = "!@#$%^&*()_+";
+    var Char = "";
 
 
 
