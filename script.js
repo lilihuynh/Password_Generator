b2b805aea926e2da1b9cee658a89fcdad4217aee
Lili Huynh <lillianhuynh312@gmail.com>
2021-12-13 19:33:32 -0600

    deleted comments in script file

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-///////////////////////////////////////////////////////////////////////
-// PASSWORD GENERATOR
-//
-// * For this assignment, you will not be changing the HTML and CSS at all.
-//
-// * You will need a generatePassword function is called when the user
-//   clicks the Generate Password button.
-//
-// * You can create other functions that are called from within
-//   generatePassword
-//
-// * Gather user input with prompt's and confirm's
 
 function generatePassword() {
 
